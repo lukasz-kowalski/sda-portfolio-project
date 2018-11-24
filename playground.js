ec41b80c5c73575e5a02dbb666cd2039731a8736
lukasz-kowalski <32309180+lukasz-kowalski@users.noreply.github.com>
2018-11-24 14:49:42 +0100

    Add files via upload

---
+++ playground.js
@@ -0,0 +1,71 @@
+// console.log('łukasz kowalski'.toUpperCase());
+// console.log('JavaScript jest mocno typowanym językiem'.replace('mocno', 'słabo'));
+// console.log('JavaScript jest językiem wysokiego poziomu'.split(' '));
+// console.log('Jestem królem front-endu!\n'.repeat(3));
+
+// console.log((234).toString(2));
+// console.log(23.2345432.toFixed(3));
+// console.log(parseFloat('54.34test'));
+// console.log(isFinite(2e134));
+
+// const expenses = 250;
+// const myName = 'Łukasz';
+// let amount = 1000;
+// console.log(expenses);
+// console.log(myName);
+// console.log(amount);
+// console.log(amount - expenses);
+
+// const name = 'Stefan';
+// const car = 'Audi';
+// const place = 'Wrocław';
+
+// console.log(name + ' kupił auto marki ' + car + '. W pierwszą trasę wybrał się do ' + place + 'ia.');
+
+// console.log(`${name} kupił auto marki ${car}. W pierwszą trasę wybrał się do ${place}ia.`);
+
+// const subtract = (a, b, c) => a - b - c;
+
+// console.log(subtract(5, 3, 2));
+// console.log(subtract(10, 2, 1));
+
+// const editText = (name, surname) => `Customer name: ${name} ${surname}`;
+
+// console.log(editText('Adam', 'Małysz'));
+// console.log(editText('Mieszko', 'Pierwszy'));
+
+// const months = ['Styczeń', 'Luty', 'Marzec', 'Kwiecień'];
+// console.log(months.join(', '));
+
+// const numbers = [4, 45, 32, 100];
+
+// console.log(numbers.filter(number => number < 10));
+
+// const newArray = numbers.map(element => element * 3);
+
+// console.log(newArray);
+
+// const car = {
+//   company: 'Audi',
+//   model: 'A3',
+//   yearOfProduction: 2013,
+//   start() {
+//     console.log('starting...');
+//   }
+// };
+
+// car.start();
+
+// class Car {
+//   constructor(company, model, year) {
+//     this.company = company;
+//     this.model = model;
+//     this.year = year;
+//   }
+//   status() {
+//     return `This auto is ${this.company} ${this.model} ${this.year}`;
+//   }
+// }
+
+// const audi = new Car('Audi', 'A3', 2013);
+// console.log(audi.status());
